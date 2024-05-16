1275775d22be1761f8a891736befa67077b139fb
willy-270 <jjarlo1985@gmail.com>
2024-05-16 00:16:22 -0500

    init

---
+++ client.py
@@ -0,0 +1,18 @@
+from consts import BOT_TOKEN
+from discord.ext import commands
+import discord
+
+bot = commands.Bot(command_prefix='', intents=discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    print('Bot is ready')
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} command(s)")
+    except Exception as e:
+        print(e)
+
+def run():
+    bot.run(BOT_TOKEN)
+    
